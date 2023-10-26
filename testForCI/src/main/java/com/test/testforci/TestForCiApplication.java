bbd6b1a4561204bd6eb445cba98d8f9e9dae9aab
김지수 <wltn2858@swu.ac.kr>
2023-10-26 17:10:46 +0900

    프로젝트 생성

---
+++ testForCI/src/main/java/com/test/testforci/TestForCiApplication.java
@@ -0,0 +1,13 @@
+package com.test.testforci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForCiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestForCiApplication.class, args);
+  }
+
+}
